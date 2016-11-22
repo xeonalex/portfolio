9d2a3eb6b1818f418ac06109efd44894a4f128e3
Sasha Saulyak <lux1995@yandex.ru>
2016-11-22 23:44:49 +0200

    galpfile v2

--- gulpfile.js
+++ gulpfile.js
@@ -13,41 +13,95 @@ var gulp = require('gulp'),
     uncss = require('gulp-uncss'), // удаление не используемых стилей
     rename = require('gulp-rename'),
     watch = require('gulp-watch'),
-    changed = require('gulp-changed');
+    changed = require('gulp-changed'),
+    rigger = require('gulp-rigger'), // іморт файлів в файл like //="../../../bower_components/...
+    gcmq = require('gulp-group-css-media-queries'), // обєднує media з однаковими breakpoint
+    zip = require('gulp-zip'); // обєднує media з однаковими breakpoint
 
-// удаление не используемых стилей  - вызывается после сборки проекта
+var path = {
+    name: "boiler",
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        server: 'build/',
+        html: 'build/',
+        js: 'build/js/',
+        jsVendor: 'build/js/vendor/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/css/fonts/',
+        favicon: 'build/favicon/'
+    },
+    src: { //Пути откуда брать исходники
+        pug: ['src/pug/*.pug','!src/pug/_*.pug'], //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/js/*.js',//В стилях и скриптах нам понадобятся только main файлы
+        jsVendor: 'src/js/vendor/*.js',//В стилях и скриптах нам понадобятся только main файлы
+        scss: 'src/sass/**/*.scss',
+        img: ['src/img/**/*.*','!src/img/**/*.tmp'],//  Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/*',
+        favicon: 'src/favicon/*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        pug: './src/pug/**/*.pug',
+        pugIncludes: './src/pug/_includes/**/*.pug',
+        js: './src/js/*.js',
+        jsVendor: './src/js/vendor/*.js',
+        scss: ["./src/sass/**/*.scss",'./src/sass/_*.scss'],
+        img: './src/img/**/*',
+        favicon: './src/favicon/*',
+        fonts: './src/fonts/*'
+    }
+};
 
+// робимо архів нашого білда
+gulp.task('zip', () => {
+    return gulp.src([path.build.server+'**/*','!'+path.build.server+'**/*.zip'])
+        .pipe(zip('build_'+path.name+'.zip'))
+        .pipe(gulp.dest(path.build.server));
+});
+// очищаемо від невикористовуваних стилів
+gulp.task('uncss', function() {
+  return gulp.src('build/css/styles.css')
+    .pipe(uncss({
+      html: ['build/*.html']
+    }))
+    //.pipe(gcmq())
+    .pipe(shorthand())
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest(path.build.css));
+});
 //Собираем Pug ( html )
 gulp.task('pug-includes', function() {
-  return gulp.src(['./src/pug/*.pug','!./src/pug/_*.pug'])
+  return gulp.src(path.src.pug)
     .pipe(plumber())
     .pipe(pug({
+       pretty: true
     }))
     .on('error', console.log)
-    .pipe(gulp.dest('./build/'))
+    .pipe(gulp.dest(path.build.html))
     .pipe(browserSync.stream());
   });
 //Собираем только изменившийся файл Pug ( html )
 gulp.task('pug-templates', function() {
-  return gulp.src(['./src/pug/*.pug','!./src/pug/_*.pug'])
-    .pipe(changed('./build/', {extension: '.html'}))
+  return gulp.src(path.src.pug)
+    .pipe(changed(path.build.html, {extension: '.html'}))
     .pipe(plumber())
     .pipe(pug({
     }))
     .on('error', console.log)
-    .pipe(gulp.dest('./build/'))
+    .pipe(gulp.dest(path.build.html))
     .pipe(browserSync.stream());
   });
 // Собираем CSS из SASS файлов
 gulp.task('sass-dev', function() {
-  return gulp.src('src/sass/**/*.scss')
+  return gulp.src(path.src.scss)
     .pipe(plumber())
     .pipe(sourcemaps.init())
+    .pipe(rigger())
     .pipe(sass({
       style: 'compressed',
       errLogToConsole: true,
       sourcemaps : false
       }))
+    .pipe(gcmq())
     .on('error', sass.logError)
     .pipe(autoprefixer({
       browsers: ['last 15 versions'],
@@ -59,61 +113,49 @@ gulp.task('sass-dev', function() {
         }
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/css/'))
+    .pipe(gulp.dest(path.build.css))
     .pipe(browserSync.stream());
 });
-gulp.task('uncss', function() {
-  return gulp.src('build/css/styles.css')
-    .pipe(uncss({
-      html: ['build/*.html']
-    }))
-    .pipe(shorthand())
-    .pipe(rename('main.css'))
-    .pipe(gulp.dest('build/css/'));
-});
-
 //Сжатие изображений
 gulp.task('img', function() {
-  return gulp.src('src/img/**/**/**')
+  return gulp.src(path.src.img)
     .pipe(imagemin({ optimizationLevel: 3, progressive: true}))
-    .pipe(gulp.dest('build/img/'));
+    .pipe(gulp.dest(path.build.img));
 });
-
 //Копируем JS
 gulp.task('js', function(){
-  return gulp.src('src/js/*.js')
+  return gulp.src(path.src.js)
   .pipe(plumber())
+  .pipe(rigger())
   .pipe(uglify())
   .pipe(concat('script.js'))
-  .pipe(gulp.dest('build/js/'))
+  .pipe(gulp.dest(path.build.js))
   .pipe(browserSync.stream());
 });
-
 //Копируем JS-vendor
 gulp.task('js-vendor', function(){
-  return gulp.src('src/js/vendor/*.js')
+  return gulp.src(path.src.jsVendor)
   .pipe(plumber())
+  .pipe(rigger())
   .pipe(uglify())
   .pipe(concat('vendor.js'))
-  .pipe(gulp.dest('build/js/vendor/'))
+  .pipe(gulp.dest(path.build.jsVendor))
   .pipe(browserSync.stream());
 });
-
 // Favicon
 gulp.task('favicon', function(){
-  return gulp.src('src/favicon/*')
-  .pipe(changed('./build/favicon/'))
+  return gulp.src(path.src.favicon)
+  .pipe(changed(path.build.favicon))
   .pipe(plumber())
-  .pipe(gulp.dest('build/favicon/'))
+  .pipe(gulp.dest(path.build.favicon))
   .pipe(browserSync.stream());
 });
-
 // Fonts
 gulp.task('fonts', function(){
-  return gulp.src('src/fonts/*')
-  .pipe(changed('./build/css/fonts/'))
+  return gulp.src(path.src.fonts)
+  .pipe(changed(path.build.fonts))
   .pipe(plumber())
-  .pipe(gulp.dest('build/css/fonts/'))
+  .pipe(gulp.dest(path.build.fonts))
   .pipe(browserSync.stream());
 });
 
@@ -121,39 +163,38 @@ gulp.task('fonts', function(){
 gulp.task('default', ['pug-includes','sass-dev','img','js-vendor','js','favicon','fonts'], function () {
 
     browserSync.init({
-      server : './build'
+      server : path.build.server
     });
-    watch('./src/pug/_includes/**/*.pug', function() {
+    watch(path.watch.pugIncludes, function() {
       gulp.start('pug-includes');
     });
 
-    watch('./src/pug/**/*.pug', function() {
+    watch(path.watch.pug, function() {
       gulp.start('pug-templates');
       // gulp.start('uncss');
     });
 
-    watch(["./src/sass/**/*.scss",'./src/sass/_*.scss'], function() {
+    watch(path.watch.scss, function() {
       gulp.start('sass-dev');
     });
 
-    watch('./src/js/*.js', function() {
+    watch(path.watch.js, function() {
       gulp.start('js');
     });
 
-    watch('./src/js/vendor/*.js', function() {
+    watch(path.watch.jsVendor, function() {
       gulp.start('js-vendor');
     });
 
-    watch('./src/img/**/*', function() {
+    watch(path.watch.img, function() {
       gulp.start('img');
     });
 
-    watch('./src/favicon/*', function() {
+    watch(path.watch.favicon, function() {
       gulp.start('favicon');
     });
 
-    watch('./src/fonts/*', function() {
+    watch(path.watch.fonts, function() {
       gulp.start('fonts');
     });
-
 });
--- src/js/vendor/vendor.js
+++ src/js/vendor/vendor.js
@@ -1,1 +1,3 @@
 console.log('hello from vendor.js');
+//="../../../bower_components/jquery/dist/jquery.js"
+//="../../../bower_components/bPopup/jquery.bpopup.js"
